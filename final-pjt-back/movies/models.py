0ba1dc64725b24bba413a5bdcb7674dd8a4a7395
이은석 <modelism93@naver.com>
2023-05-17 23:19:40 +0900

    add : movie_data json파일로 받아오기

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -3,11 +3,12 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
 
+    movie_id = models.IntegerField()
     title = models.TextField()
+    released_date = models.DateField()
     poster_path = models.TextField()
     vote_count = models.IntegerField()
     vote_average = models.IntegerField()
-    backdrop_path=models.TextField()
-    genre_ids=models.JSONField()
-    
-    
+    overview = models.TextField()
+    genres=models.JSONField()
+    # backdrop_path=models.TextField()
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -8,33 +8,53 @@ from .models import *
 import requests
 import json
 
-url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=ko-KR&page=1&sort_by=popularity.desc"
 
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjNlM2FhNjVlOTZlOTVjN2Y0MWZmMDdmY2NkMzAxYiIsInN1YiI6IjYzZDMxYTM4NWEwN2Y1MDA5ZTk4MDM0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.svzZx8RMTp1kjkBzxbvcOuoduFUJjduTqyQf-ufCBfo"
-}
+def get_movie_datas():
+    total_data = []
 
-Movie_json_list = []
-response = requests.get(url, headers=headers)
+    for i in range(1,20):
+        url = f"https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-KR&page={i}&sort_by=popularity.desc"
 
-responsejson = (response.json())
+        headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YjNlM2FhNjVlOTZlOTVjN2Y0MWZmMDdmY2NkMzAxYiIsInN1YiI6IjYzZDMxYTM4NWEwN2Y1MDA5ZTk4MDM0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.svzZx8RMTp1kjkBzxbvcOuoduFUJjduTqyQf-ufCBfo"
+        }
 
-print(responsejson['results'])
-print(type(responsejson['results']))
+        response = requests.get(url, headers=headers).json()
 
+        for movie in response['results']:
+            if movie.get('release_date', ''):
+                fields = {
+                    'movie_id': movie['id'],
+                    'title' : movie['title'], 
+                    'released_date': movie['release_date'],
+                    'poster_path':movie['poster_path'],
+                    'vote_count':movie['vote_count'],
+                    'vote_average': movie['vote_average'],
+                    'overview': movie['overview'],
+                    'poster_path': movie['poster_path'],
+                    'genres': movie['genre_ids']
+                }
+                data = {
+                    'pk' : movie['id'],
+                    'model': 'movies.movie',
+                    'fields': fields
+                }
+
+                total_data.append(data)
+
+        with open("movies/fixtures/movie_data.json", "w", encoding="utf-8") as w:
+            json.dump(total_data, w, indent="\t", ensure_ascii=False)
+        
+        
+
+
+get_movie_datas()
 
 
 # Create your views here.
 
-@api_view(['GET'])
-def movies_list(request):
-    if request.method == 'GET':
-        
-        movies = responsejson['results']
-        
-        
-        serializer = MovieSerializer(movies, many=True)
-        # if serializer.is_valid():
-        #     serializer.save()
-        return Response(serializer.data)
+# @api_view(['GET'])
+# def movies_list(request):
+#     if request.method == 'GET':   
+#         return
