0a93520d96849be3d650f09da9464f3ad7866612
EunSeok <modelism93@naver.com>
2023-05-18 10:50:56 +0900

    [add] comment 모델, 함수 추가

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -5,5 +5,7 @@ app_name = 'movies'
 urlpatterns =[
     path('movies/', views.movie_list),
     path('articles/', views.article_list),
-    path('articles/<int:article_pk>/', views.article_detail)
-]
+    path('articles/<int:article_pk>/', views.article_detail),
+    path('comments/<int:comment_pk>/', views.comment_detail),
+    path('articles/<int:article_pk>/comments/', views.comment_create),
+]   
