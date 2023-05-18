f13971d8b3fd31ffcbcbcb34af8de1de04f7fed7
EunSeok <modelism93@naver.com>
2023-05-18 10:09:49 +0900

    [add] 게시글 수정, 삭제 기능 추가

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -75,9 +75,19 @@ def article_list(request):
             # serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET','PUT','DELETE'])
 def article_detail(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    
+    if request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
