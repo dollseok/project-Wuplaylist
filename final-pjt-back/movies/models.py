fecb684b3333a7481c3f23f3c7a9ba736755c200
이은석 <modelism93@naver.com>
2023-05-20 21:08:35 +0900

    [Add] Article, Comment_article model에 user 추가

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Movie(models.Model):
@@ -16,13 +17,15 @@ class Movie(models.Model):
 
 class Article(models.Model):
     
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment_article(models.Model):
-    
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)    
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
