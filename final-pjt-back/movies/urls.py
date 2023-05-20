dd28e2325127bb6c121dbb1a3e42547aa6a65764
이은석 <modelism93@naver.com>
2023-05-20 21:03:20 +0900

    주석 제거

--- final-pjt-back/movies/urls.py
+++ final-pjt-back/movies/urls.py
@@ -11,17 +11,18 @@ urlpatterns =[
     path('articles/', views.article_list),
     path('articles/<int:article_pk>/', views.article_detail),
     
+
     # 게시글 댓글 CRUD url
     # 게시글 댓글 상세 조회, 수정, 삭제 
     path('comments_article/<int:comment_pk>/', views.comment_article_detail),
     # 게시글 댓글 리스트 조회, 생성
     path('articles/<int:article_pk>/comments_article/', views.comment_list_article),
-    # path('articles/<int:article_pk>/comments_article/', views.comment_article_create),
+    
 
     # 영화 댓글 CRUD url
     # 영화 댓글 상세 조회, 수정, 삭제
     path('comments_movie/<int:comment_pk>/', views.comment_movie_detail),
     # 영화 댓글 리스트 조회, 생성
     path('movies/<int:movie_pk>/comments_movie/', views.comment_list_movie),
-    # path('movies/<int:movie_pk>/comments_movie/', views.comment_movie_create),
+    
 ]   
