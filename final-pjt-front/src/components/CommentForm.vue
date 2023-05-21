fc304b17c450d257794705e4f081f2a4a8adffcd
이은석 <modelism93@naver.com>
2023-05-21 14:21:56 +0900

    [Fix] 댓글 작성 안되는 것 수정

--- final-pjt-front/src/components/CommentForm.vue
+++ final-pjt-front/src/components/CommentForm.vue
@@ -28,6 +28,10 @@ export default {
         method: 'post',
         url: `${API_URL}/api/v1/articles/${this.articleId}/comments_article/`,
         data: { content },
+        // headers 추가로 댓글 작성 가능하게 함(user data의 요구)
+        headers: {
+          Authorization: `Token ${ this.$store.state.token }`
+        }
       })
       .then(() => {
         // 인자로 넘긴 숫자만큼 히스토리 스택에서 앞, 뒤 페이지로 이동
--- final-pjt-front/src/views/reviews/CreateView.vue
+++ final-pjt-front/src/views/reviews/CreateView.vue
@@ -25,7 +25,7 @@ export default {
     return {
       title: null,
       content: null,
-      user : null,
+      // user : null,
     }
   },
   methods: {
