7917547fc56763ff9277930145c6e93546446ef6
EunSeok <modelism93@naver.com>
2023-05-23 17:50:02 +0900

    [add] Genre buttons 추가 (ing)

--- final-pjt-front/src/components/GenreList.vue
+++ final-pjt-front/src/components/GenreList.vue
@@ -1,16 +1,42 @@
 <template>
   <div>
     <h1>Choice Genre</h1>
-
+    <div class="genre">
+      <div
+        v-for="genre in genres" :key="genre.id"
+        class="genre_button"
+      >
+        {{ genre.genre_name }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'GenreList',
+    data(){
+      return {}
+    },
+    computed :{
+      genres(){
+        return this.$store.state.genres
+      }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+.genre {
+  display: flex;
+  flex-wrap: wrap;
+}
+.genre_button{
+  margin: 10px;
+  width: 70px;
+  height: 50px;
+  background-color: yellow;
+
+}
 
 </style>
