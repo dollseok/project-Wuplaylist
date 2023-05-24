ec5dab206ab642e3ba4227ecf8fe8162fc40ffd6
이은석 <modelism93@naver.com>
2023-05-24 21:36:41 +0900

    [add] 인기 플레이리스트 정렬해서 출력

---
+++ final-pjt-front/src/components/FollowPlaylist.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>팔로우 플레이리스트</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="home">
-    <h1>HomeView</h1>
     <AllMovieList />
     <!-- 추천 플레이리스트 (좋아요 순으로 출력하기) -->
     
-    <div class="recommend-playlist">
-      <ReviewPopular />
+    <div class="menu2">
+
+        <div class="recommend-playlist">
+          <ReviewPopular />
+        </div>
+        <div class="follow-playlist">
+          <FollowPlaylist/>
+        </div>
+
     </div>
   </div>
 </template>
@@ -13,12 +19,14 @@
 <script>
 import AllMovieList from '@/components/AllMovieList.vue'
 import ReviewPopular from '@/components/ReviewPopular.vue'
+import FollowPlaylist from '@/components/FollowPlaylist.vue'
 
 export default {
   name: 'HomeView',
   components: {
     AllMovieList,
     ReviewPopular,
+    FollowPlaylist,
   },
   created() {
     this.getAllMovies()
@@ -37,3 +45,26 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.menu2 {
+  display: flex;
+}
+
+.recommend-playlist{
+  padding: 10px;
+  width:50%;
+  border: 1px solid black;
+  margin: 10px;
+  border-radius: 10px;
+}
+.follow-playlist{
+  padding: 10px;
+  width:50%;
+  border: 1px solid black;
+  margin: 10px;
+  border-radius: 10px;
+}
+
+
+</style>
